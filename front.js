5b022e8278ba676ebdd88e3ca4ce6f8e2be4ca08
pratyush-kr <pratyush151198@gmail.com>
2021-11-10 11:15:51 +0530

    db

--- front.js
+++ front.js
@@ -7,3 +7,10 @@ function openForm(){
 function closeForm(){
     document.getElementById("form").style.display = "none";
 }
+
+function handelSubmit(event){
+    event.preventDefault();
+    var myForm = document.getElementById('myLoginForm');
+    const data = new FormData(myForm);
+    console.log(data);
+}
--- server.js
+++ server.js
@@ -4,3 +4,11 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 var db = mongoose.connect("mongoose://localhost/PlantDB");
+
+app.listen(7870, function(){
+    console.log("Server is up and running");
+});
+
+app.post('/login', function(req, res){
+    var form = document.getElementById('form');
+});
